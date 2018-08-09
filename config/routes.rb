077845ca39829c3d314de207ca37ba3e1f069445
Joy Seng <allthatjoy@gmail.com>
2018-08-09 12:53:26 +1000

    cocktails internal piping working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ingredients/index'
+  get 'ingredients/show'
+  get 'ingredients/new'
+  get 'ingredients/create'
+  get 'ingredients/edit'
+  get 'ingredients/update'
+  get 'ingredients/destroy'
+  resources :cocktails
+  resources :ingredients
 end
